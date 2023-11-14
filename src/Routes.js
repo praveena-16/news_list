e75885c81e625bd132605979b43a402a2aed7962
praveena-cp <praveena.m@cp-360.com>
2023-11-14 23:34:22 +0530

    [All List]

--- src/Routes.js
+++ src/Routes.js
@@ -1,11 +1,10 @@
 import React from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./Home";
 
 const routes = (
   <Routes>
     <Route path="/" element={<Home />} />
-    <Route path="*" element={<Navigate to="/404" />} />
   </Routes>
 );
 
